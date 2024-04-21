25b873c8164e3ad8d6b2e5cc0143976ce7b568df
Jiwoo Jeong <98355440+erase-jeong@users.noreply.github.com>
2024-04-21 23:21:09 +0900

    commit

--- textAnalyzerPy/textAnalyzerPy.py
+++ textAnalyzerPy/textAnalyzerPy.py
@@ -1,28 +1,25 @@
 import re
 from collections import Counter
 
-class TextAnalyzer:
-    def __init__(self, text):
-        self.text = text
-        self.words = self._words()
+def extract_words(text):
+    # 텍스트에서 단어만 추출하여 반환
+    words = re.findall(r'\b\w+\b', text.lower())
+    return words
 
-    def _words(self):
-        # 텍스트에서 단어만 추출
-        words = re.findall(r'\b\w+\b', self.text.lower())
-        return words
+def most_common_words(text, num=10):
+    # 주어진 텍스트에서 가장 많이 나타나는 단어를 찾기
+    words = extract_words(text)
+    count = Counter(words)
+    return count.most_common(num)
 
-    def most_common_words(self, num=10):
-        # 가장 많이 나타나는 단어를 찾기
-        count = Counter(self.words)
-        return count.most_common(num)
-
-    def text_complexity(self):
-        # 텍스트의 복잡성을 평가
-        num_words = len(self.words)
-        unique_words = len(set(self.words))
-        lex_diversity = unique_words / num_words
-        return {
-            "total_words": num_words,
-            "unique_words": unique_words,
-            "lexical_diversity": lex_diversity
-        }
+def text_complexity(text):
+    # 텍스트의 복잡성을 평가
+    words = extract_words(text)
+    num_words = len(words)
+    unique_words = len(set(words))
+    lex_diversity = unique_words / num_words
+    return {
+        "total_words": num_words,
+        "unique_words": unique_words,
+        "lexical_diversity": lex_diversity
+    }
